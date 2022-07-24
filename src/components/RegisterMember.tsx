71a76b2f0c8bf9c59f3b67ae601ff7b7cc73b33b
malex <ma.lex@outlook.com>
2022-07-23 23:06:36 -0700

    changed member input form to be a modal using daisyUI pending making it work

--- src/components/RegisterMember.tsx
+++ src/components/RegisterMember.tsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Team } from '../models/Team';
 
 const RegisterMember = () => {
+
   const [name, setName] = useState('');
 
   const handleSubmit = (submitEvent: React.FormEvent<HTMLFormElement>) => {
@@ -29,12 +30,19 @@ const RegisterMember = () => {
   };
 
   return (
-    <div >
-      <form className="grid gird-columns-auto p-2 w-1/2" onSubmit={handleSubmit}>
-        <label>Name:</label>
-        <input name="name" className="p-3 border-solid border-indigo-600" type="text" onChange={handleChange}/>
-        <input className="p-3 form-input rounded-md" type="submit" value="Register"/>
-      </form>
+    <div>
+      <label htmlFor="register-member-form" className="btn modal-button">Register new member</label>
+      <input type="checkbox" id="register-member-form" className="modal-toggle" />
+      <div className="modal">
+        <div className="modal-box relative">
+          <label htmlFor="register-member-form" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+          <form className="grid grid-columns-auto p-2 w-1/2" onSubmit={handleSubmit}>
+            <label>Name:</label>
+            <input name="name" className="p-3 border-solid border-indigo-600" type="text" onChange={handleChange} />
+            <input className="p-3 form-input rounded-md" type="submit" value="Register" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
